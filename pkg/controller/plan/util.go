05bd970bb096eac4d9721c2857f024be1e613b67
Tuna Batikan Acikbas <batikan.acikbas@agoracalyce.com>
2026-06-16 18:07:32 +0200

    Added Namespace existant verification

--- pkg/controller/plan/util.go
+++ pkg/controller/plan/util.go
@@ -10,17 +10,37 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+
+func namespaceExists(name string, cl client.Client) (bool, error) {
+   ns := &core.Namespace{}
+   err := cl.Get(context.TODO(), client.ObjectKey{Name: name}, ns)
+   if err != nil {
+      if k8serr.IsNotFound(err) {
+          return false, nil
+      }
+      return false, err
+   }
+   return true, nil
+}
 // Ensure the namespace exists on the destination.
 func ensureNamespace(plan *api.Plan, client client.Client) error {
 	ns := &core.Namespace{
 		ObjectMeta: meta.ObjectMeta{
 			Name: plan.Spec.TargetNamespace,
 		},
-	}
-	err := client.Create(context.TODO(), ns)
-	if err != nil && k8serr.IsAlreadyExists(err) {
-		err = nil
-	}
+
+    exists, err := namespaceExists(plan.Spec.TargetNamespace, client)
+    if err != nil {
+        return err
+    }
+    if exists {
+        return nil
+    }
+    if err != nil && k8serr.IsNotFound(err){
+        if err != nil && k8serr.IsAlreadyExists(err) {
+         err = nil
+        }
+    }
 	return err
 }
 
